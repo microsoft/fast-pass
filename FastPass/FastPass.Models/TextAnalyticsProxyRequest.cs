5e91ad591ad57d1eab7144f18d00762aabf2f34a
David Ginn <davidmginn@gmail.com>
2022-11-15 10:59:40 -0500

    integration with function app

--- FastPass/FastPass.Models/TextAnalyticsProxyRequest.cs
+++ FastPass/FastPass.Models/TextAnalyticsProxyRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace FastPass.Models
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using ResourceType = Hl7.Fhir.Model.ResourceType;
+
+namespace FastPass.Models
 {
     public class TextAnalyticsProxyRequest
     {
 
+        [Required]
         public string? TextToAnalize { get; set; }
         public string? Id { get; set; }
         public string? Language { get; set; }
