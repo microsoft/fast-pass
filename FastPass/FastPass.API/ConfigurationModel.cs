74222f80461e551ce29a9f87f35b244e46e181c2
mjschanne <mjschanne@microsoft.com>
2022-11-17 16:05:17 -0500

    Add FirelyService

--- FastPass/FastPass.API/ConfigurationModel.cs
+++ FastPass/FastPass.API/ConfigurationModel.cs
@@ -5,4 +5,8 @@ public class ConfigurationModel
     public const string Section = "APIConfig";
     public string TextAnalyticsBase { get; set; } = string.Empty;
     public string TextAnalyticsKey { get; set; } = string.Empty;
+
+    public string FhirScope { get; set; } = string.Empty;
+    public string FhirServerUri { get; set; } = string.Empty;
+    public string Authority { get; set; } = string.Empty;
 }
---
+++ FastPass/FastPass.API/Services/AuthorizationHandler.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FastPass.API.Services;
+
+public class AuthorizationHandler : HttpClientHandler
+{
+    private static DefaultAzureCredential _credentials = new DefaultAzureCredential();
+    internal string[] Scopes;
+
+    public AuthenticationHeaderValue Authorization { get; set; }
+    protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var ctx = new TokenRequestContext(Scopes);
+
+        var token = await _credentials.GetTokenAsync(ctx);
+
+        Console.Write(token.ToString());
+
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.Token);
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
