0c40cdfc9acef46329de63c4c372a0cd7fcf9930
Chad Voelker <chad.voelker@microsoft.com>
2022-11-18 11:49:08 -0600

    FhirService Round Trip

---
+++ FastPass/FastPass.API/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FastPass.API
+{
+    public static class ExtensionMethods
+    {
+        public static async Task<string> GetBodyString(this HttpRequest req)
+        {
+            string bodyString;
+            using (var sr = new StreamReader(req.Body))
+            {
+                bodyString = await sr.ReadToEndAsync();
+            }
+            return bodyString;
+        }
+    }
+}
---
+++ FastPass/FastPass.API/TextAnalyticsModels/UpdatePatientRequest.cs
@@ -0,0 +1,10 @@
+﻿using Hl7.Fhir.Model;
+
+namespace FastPass.API.TextAnalyticsModels
+{
+    public class UpdatePatientRequest
+    {
+        public Patient Patient { get; set; }
+        public string Id { get; set;}
+    }
+}
