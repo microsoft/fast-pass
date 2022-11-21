c1bc1a0f6656e3d35424da6a3ce4b5568b4a8caf
mjschanne <mjschanne@microsoft.com>
2022-11-21 18:34:36 -0500

    Add ApiService to UI

--- FastPass/FastPass.UI/Program.cs
+++ FastPass/FastPass.UI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using AzureStaticWebApps.Blazor.Authentication;
 using Blazored.SessionStorage;
 using FastPass.UI;
+using FastPass.UI.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,11 +9,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-string baseUrl = builder.HostEnvironment.IsDevelopment() ? "http://localhost:7214/" : builder.HostEnvironment.BaseAddress;
+var baseUrl = builder.HostEnvironment.IsDevelopment() ? "http://localhost:7214/" : builder.HostEnvironment.BaseAddress;
 
 builder.Services.AddScoped(sp => {
     return new HttpClient { BaseAddress = new Uri(baseUrl) };
 });
+builder.Services.AddScoped<ApiService>();
 
 builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddStaticWebAppsAuthentication();
