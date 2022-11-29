57f6a91aa2ca601bef406c13bc5ab4ceaff4cddf
mjschanne <mjschanne@microsoft.com>
2022-11-29 11:26:27 -0500

    Add components for displaying Observation data

--- FastPass/FastPass.UI/Pages/FindFhirData.razor.cs
+++ FastPass/FastPass.UI/Pages/FindFhirData.razor.cs
@@ -4,27 +4,29 @@ namespace FastPass.UI.Pages;
 
 public partial class FindFhirData
 {
-
     private Bundle FhirBundle { get; set; } = default!;
 
-    private List<Patient> Patients
+    private List<Patient?> Patients
     {
         get
         {
             return FhirBundle.Entry
-        ?.Where(e => string.Equals(e?.Resource?.TypeName, "patient", StringComparison.OrdinalIgnoreCase) && e.Resource is Patient)
-        ?.Select(e => e.Resource as Patient)
-        ?.Where(p => p != null)
-        ?.ToList() ?? new List<Patient>();
+                ?.Where(e => string.Equals(e?.Resource?.TypeName, "patient", StringComparison.OrdinalIgnoreCase) && e?.Resource is Patient)
+                ?.Select(e => e.Resource as Patient)
+                ?.Where(p => p != null)
+                ?.ToList() ?? new List<Patient?>();
         }
     }
 
-    //protected override async Task OnInitializedAsync()
-    //{
-    //}
-
-
-    /*
-            var observations = bundle.Entry.Where(x => x.Resource.TypeName.ToLower() == "observation").Select(x => (Observation)x.Resource).ToList();
-     */
+    private List<Observation?> Observations
+    {
+        get
+        {
+            return FhirBundle.Entry
+                ?.Where(entryComponent => entryComponent.Resource.TypeName.ToLower() == "observation")
+                ?.Select(entryComponent => entryComponent?.Resource as Observation)
+                ?.Where(o => o != null)
+                ?.ToList() ?? new List<Observation?>();
+        }
+    }
 }
